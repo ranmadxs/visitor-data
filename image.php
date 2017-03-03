40977d89be21d0ef185710c249a58929d77816f3
Edgar Sanchez <grineldosanchez@yahoo.es>
2017-03-03 01:28:14 -0300

    Listo

--- image.php
+++ image.php
@@ -1,14 +1,14 @@
 <?php
-//include("geoiploc.php");
 require_once dirname(__FILE__).'/../lib/apache-log4php-2.3.0/src/main/php/Logger.php';
 Logger::configure(dirname(__FILE__).'/resources/appender_dailyfile.properties');
 
 $logger = Logger::getRootLogger();
 
 header ('Content-Type: image/png');
-$im = @imagecreatetruecolor(120, 20)
+$im = @imagecreatetruecolor(1, 1)
 or die('No se puede Iniciar el nuevo flujo a la imagen GD');
-$color_texto = imagecolorallocate($im, 233, 14, 91);
+$fondo = imagecolorallocate($im, 255, 255, 255);
+$color_texto = imagecolorallocate($im, 0, 0, 255);
 $ip = $_SERVER["REMOTE_ADDR"];
 if (isset($_SERVER["HTTP_FORWARDED"])){
 	$ip = $_SERVER["HTTP_FORWARDED"];
@@ -18,10 +18,12 @@ if (isset($_SERVER["HTTP_X_FORWARDED_FOR"])){
 }
 
 $meta = unserialize(file_get_contents('http://www.geoplugin.net/php.gp?ip='.$ip));
-$logger->info("  >>>>>    IP:".$ip."    <<<<<  ");
+$logger->info("  >>>>>   INICIO IP:".$ip."    <<<<<  ");
 $logger->info($_SERVER);
 $logger->info($meta);
-imagestring($im, 1, 5, 5,  $_SERVER["REMOTE_ADDR"], $color_texto);
+$logger->info("  >>>>>  FIN  IP:".$ip."    <<<<<  ");
+imagestring($im, 2, 5, 5,  "", $color_texto);
+imagefill($im, 0, 0, $fondo);
 imagepng($im);
 imagedestroy($im);
 ?>
--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
 include_once '../lib/dpr.php';
+/*
+include_once '../lib/dpr.php';
 //dpr("XD");
 $meta = unserialize(file_get_contents('http://www.geoplugin.net/php.gp?ip='."190.164.212.151"));
 dpr($meta);
@@ -9,4 +11,5 @@ $longitud = $meta['geoplugin_longitude'];
 $ciudad = $meta['geoplugin_city'];
 
 dpr($_SERVER);
+*/
 ?>
