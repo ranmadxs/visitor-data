53c90d75148996ce21927aa1be5000c7bfd8ca48
Edgar Sanchez <grineldosanchez@yahoo.es>
2017-03-03 00:17:22 -0300

    image.php

--- image.php
+++ image.php
@@ -1,4 +1,5 @@
 <?php
+//include("geoiploc.php");
 require_once dirname(__FILE__).'/../lib/apache-log4php-2.3.0/src/main/php/Logger.php';
 Logger::configure(dirname(__FILE__).'/resources/appender_dailyfile.properties');
 
@@ -8,8 +9,10 @@ header ('Content-Type: image/png');
 $im = @imagecreatetruecolor(120, 20)
 or die('No se puede Iniciar el nuevo flujo a la imagen GD');
 $color_texto = imagecolorallocate($im, 233, 14, 91);
-$jsonData = '{"REMOTE_ADDR":"'.$_SERVER["REMOTE_ADDR"].'", "HTTP_USER_AGENT":'.$_SERVER['HTTP_USER_AGENT'].'}';
+$jsonData = '{"REMOTE_ADDR":"'.$_SERVER["REMOTE_ADDR"].'", "HTTP_USER_AGENT":"'.$_SERVER['HTTP_USER_AGENT'].'"}';
+$meta = unserialize(file_get_contents('http://www.geoplugin.net/php.gp?ip='.$_SERVER["REMOTE_ADDR"]));
 $logger->info($jsonData);
+$logger->info($meta);
 imagestring($im, 1, 5, 5,  $_SERVER["REMOTE_ADDR"], $color_texto);
 imagepng($im);
 imagedestroy($im);
--- index.php
+++ index.php
@@ -1,5 +1,12 @@
 <?php
 include_once '../lib/dpr.php';
 
+$meta = unserialize(file_get_contents('http://www.geoplugin.net/php.gp?ip='."190.164.212.151"));
+echo (json_encode($meta));
+
+$latitud = $meta['geoplugin_latitude'];
+$longitud = $meta['geoplugin_longitude'];
+$ciudad = $meta['geoplugin_city'];
+
 dpr($_SERVER);
 ?>
